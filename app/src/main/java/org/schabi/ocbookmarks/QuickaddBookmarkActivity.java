1e50127bc7971af016ee04d051d5c6e1804a873b
Felix Nüsse <felix.nuesse@t-online.de>
2023-01-31 21:57:01 +0100

    Replace Async-Task
    Signed-off-by: Felix Nüsse <felix.nuesse@t-online.de>

--- app/src/main/java/org/schabi/ocbookmarks/QuickaddBookmarkActivity.java
+++ app/src/main/java/org/schabi/ocbookmarks/QuickaddBookmarkActivity.java
@@ -3,6 +3,8 @@ package org.schabi.ocbookmarks;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -20,6 +22,9 @@ import com.nextcloud.android.sso.model.SingleSignOnAccount;
 
 import org.schabi.ocbookmarks.REST.Bookmark;
 import org.schabi.ocbookmarks.REST.OCBookmarksRestConnector;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class QuickaddBookmarkActivity extends AppCompatActivity {
 
@@ -45,54 +50,50 @@ public class QuickaddBookmarkActivity extends AppCompatActivity {
         bookmark.setTags(new String[] {this.getString(R.string.share_target_quick)});
 
 
-        AsyncTask<Void, Void, String> updateTask = new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                NextcloudAPI nextcloudAPI = null;
-                try {
-                    SingleSignOnAccount ssoa =  SingleAccountHelper.getCurrentSingleSignOnAccount(QuickaddBookmarkActivity.this.getApplicationContext());
-                    nextcloudAPI = SSOUtil.getNextcloudAPI(QuickaddBookmarkActivity.this, ssoa);
-                } catch (NextcloudFilesAppAccountNotFoundException e) {
-                    Toast.makeText(QuickaddBookmarkActivity.this,
-                            R.string.nextcloud_files_app_account_not_found_message,
-                            Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
-                    return null;
-                } catch ( NoCurrentAccountSelectedException e) {
-                    Toast.makeText(QuickaddBookmarkActivity.this,
-                            R.string.no_current_account_selected_exception_message,
-                            Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
-                    return null;
-                }
 
-                OCBookmarksRestConnector connector = new OCBookmarksRestConnector(nextcloudAPI);
-                try {
-                    connector.addBookmark(bookmark);
-                } catch (Exception e) {
-                    if(BuildConfig.DEBUG) e.printStackTrace();
-                    return getString(R.string.could_not_add_bookmark);
-                }
-                return null;
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(() -> {
+
+            String result = getString(R.string.bookmark_saved);
+            NextcloudAPI nextcloudAPI = null;
+            try {
+                SingleSignOnAccount ssoa =  SingleAccountHelper.getCurrentSingleSignOnAccount(QuickaddBookmarkActivity.this.getApplicationContext());
+                nextcloudAPI = SSOUtil.getNextcloudAPI(QuickaddBookmarkActivity.this, ssoa);
+            } catch (NextcloudFilesAppAccountNotFoundException e) {
+                Toast.makeText(QuickaddBookmarkActivity.this,
+                        R.string.nextcloud_files_app_account_not_found_message,
+                        Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            } catch ( NoCurrentAccountSelectedException e) {
+                Toast.makeText(QuickaddBookmarkActivity.this,
+                        R.string.no_current_account_selected_exception_message,
+                        Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
             }
 
-            @Override
-            protected void onPostExecute(String result) {
+            OCBookmarksRestConnector connector = new OCBookmarksRestConnector(nextcloudAPI);
+            try {
+                connector.addBookmark(bookmark);
+            } catch (Exception e) {
+                if(BuildConfig.DEBUG) e.printStackTrace();
+                result = getString(R.string.could_not_add_bookmark);
+            }
+            String finalResult = result;
+            handler.post(() -> {
                 progressView.setVisibility(View.GONE);
                 successView.setVisibility(View.VISIBLE);
-                stateView.setText(getString(R.string.bookmark_saved));
-                if(result != null) {
-                    Toast.makeText(QuickaddBookmarkActivity.this,
-                            result,
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(QuickaddBookmarkActivity.this,
-                            R.string.bookmark_saved,
-                            Toast.LENGTH_SHORT).show();
-                }
+                stateView.setText(finalResult);
+
+                Toast.makeText(QuickaddBookmarkActivity.this,
+                        finalResult,
+                        Toast.LENGTH_LONG).show();
                 finish();
-            }
-        }.execute();
+            });
+        });
+
+
 
 
     }
