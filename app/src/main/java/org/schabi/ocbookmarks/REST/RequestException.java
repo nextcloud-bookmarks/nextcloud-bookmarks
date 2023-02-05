a79fffeea13d5346ce31e7ce41efcc8e4c27a2fa
Nextcloud Bookmarks <36512-bisasda@users.noreply.gitlab.com>
2023-02-05 20:20:56 +0000

    * Error message for Bookmark app is not installed Issue [#39](https://gitlab.com/bisada/OCBookmarks/-/issues/39) * Account switching issue [#79](https://gitlab.com/bisada/OCBookmarks/-/issues/79)

--- app/src/main/java/org/schabi/ocbookmarks/REST/RequestException.java
+++ app/src/main/java/org/schabi/ocbookmarks/REST/RequestException.java
@@ -3,9 +3,27 @@ package org.schabi.ocbookmarks.REST;
 import java.io.IOException;
 
 /**
+ *
+ * Todo: Phase out the String message and try to use enum.
+ * Reason: I think it makes handling errors more easy because we dont rely on string parsing.
  * Created by the-scrabi on 14.05.17.
  */
 public class RequestException extends IOException {
+
+
+    public enum ERROR {
+        UNKNOWN,
+        API_NOT_SET_UP,
+        FILE_NOT_FOUND,
+        HOST_NOT_FOUND,
+        TIME_OUT,
+        BOOKMARK_NOT_INSTALLED
+    }
+
+    private ERROR mError = ERROR.UNKNOWN;
+
+
+    @Deprecated
     RequestException(String message, Exception e) {
         super(message, e);
     }
@@ -14,7 +32,24 @@ public class RequestException extends IOException {
         super(e);
     }
 
+
+    @Deprecated
     RequestException(String message) {
         super(message);
+    }
+
+    @Deprecated
+    RequestException(String message, ERROR error) {
+        super(message);
+        mError = error;
+    }
+
+    RequestException(ERROR error) {
+        super(error.name());
+        mError = error;
+    }
+
+    public ERROR getError() {
+        return mError;
     }
 }
